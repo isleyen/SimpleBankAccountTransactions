fdf6aa80b5c3d79eddc1d242b4e7f19e642bcd47
ahmetisleyen <ahmetstdy00@gmail.com>
2024-05-26 19:04:53 +0300

    readMeIcınGuncelleme

--- bankAccount/main.cpp
+++ bankAccount/main.cpp
@@ -8,13 +8,8 @@ void main() {
 	hesap hesap1(5000, "Ahmet Isleyen", 19);
 	hesap1.printHesap();
 	cout << "\n";
-	//hesap1.cekHesap(700);
-	//hesap1.printHesap();
-	//cout << "\n";
-	hesap1.cekHesap(4500);
+	hesap1.cekHesap(3500);
 	hesap1.printHesap();
 	cout << "\n";
-	//hesap1.yatirHesap(1400);
-	//hesap1.printHesap();
-	//cout << "\n";
+	
 }
